d66a9785576c01b1382aeb044ec252ca28e7302f
Luke <Luke.Rigby@mediacom.com>
2014-07-22 12:50:47 +0100

    updated method names and parameters

--- trolly/member.py
+++ trolly/member.py
@@ -20,35 +20,45 @@ class Member(TrelloObject):
 
         self.base_uri = '/members/' + self.id
 
-    def getMemberInformation(self, query_params={}):
+    def get_member_information(self, query_params=None):
         """
-        Get Information for a memeber. Returns a dictionary of values.
+        Get Information for a member. Returns a dictionary of values.
         """
-        return self.fetchJson(
+        return self.fetch_json(
             uri_path=self.base_uri,
-            query_params=query_params
+            query_params=query_params or {}
         )
 
-    def getBoards(self):
+    def get_boards(self):
         """
         Get all boards this member is attached to. Returns a list of Board objects.
         """
-        boards = self.getBoardsJson(self.base_uri)
+        boards = self.get_boards_json(self.base_uri)
 
         boards_list = []
         for board_json in boards:
-            boards_list.append(self.createBoard(board_json))
+            boards_list.append(self.create_board(board_json))
 
         return boards_list
 
-    def getCards(self):
+    def get_cards(self):
         """
         Get all cards this member is attached to. Return a list of Card objects.
         """
-        cards = self.getCardsJson(self.base_uri)
+        cards = self.get_cards_json(self.base_uri)
 
         cards_list = []
         for card_json in cards:
-            cards_list.append(self.createCard(card_json))
+            cards_list.append(self.create_card(card_json))
 
         return cards_list
+
+    # Deprecated
+    def getMemberInformation(self, query_params=None):
+        return self.get_member_information(query_params)
+
+    def getBoards(self):
+        return self.get_boards()
+
+    def getCards(self):
+        return self.get_cards()
